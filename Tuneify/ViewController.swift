6cb88a95cdd6606089d24083512289ed0e76bc5c
Mitchell Hamm <mitchell.hamm@Mitchells-MacBook-Pro.local>
2016-07-21 16:28:46 -0500

    more initial setup

--- Tuneify/ViewController.swift
+++ Tuneify/ViewController.swift
@@ -8,17 +8,34 @@
 
 import UIKit
 import Beethoven
+import Pitchy
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, PitchEngineDelegate {
+    
+    var pitchEngine: PitchEngine = PitchEngine(config: Config(
+        bufferSize: 4096,
+        transformStrategy: .FFT,
+        estimationStrategy: .HPS,
+        audioURL: nil))
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.pitchEngine.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func pitchEngineDidRecievePitch(pitchEngine: PitchEngine, pitch: Pitch) {
+        //Delegate
+        NSLog("pitch : \(pitch.note.string) - percentage : \(pitch.closestOffset.percentage)")
+    }
+    
+    func pitchEngineDidRecieveError(pitchEngine: PitchEngine, error: ErrorType) {
+        //Delegate
     }
 
 
