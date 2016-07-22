ae611fa88f31e7f0ff8fde0068229f45406da4c3
John Larmie <john.larmie@tippingcanoe.com>
2016-07-22 12:16:13 -0500

    key retriever

---
+++ Tuneify/KeyRetriever.swift
@@ -0,0 +1,91 @@
+//
+//  KeyRetriever.swift
+//  Tuneify
+//
+//  Created by John Larmie on 2016-07-22.
+//  Copyright © 2016 Mitchell Hamm. All rights reserved.
+//
+
+import Foundation
+
+
+class KeyRetriever {
+    
+    var keyList: [String: [String]] = [
+        "C-Major": ["C","D", "E", "F", "G", "A", "B"],
+        "G-Major": ["G","A", "B", "C", "D", "E", "F#"],
+        "D-Major": ["D","E", "F#", "G", "A", "B", "C#"],
+        "A-Major": ["A","B", "C#", "D", "E", "F#", "G#"],
+        "E-Major": ["E","F#", "G#", "A", "B", "C#", "D#"],
+        "B-Major": ["B","C#", "D#", "E", "F#", "G#", "A#"],
+        "F#-Major": ["F#","G#", "A#", "B", "C#", "D#", "E#"],
+        "Db-Major": ["Db","Eb", "F", "Gb", "Ab", "Bb", "C"],
+        "Ab-Major": ["Ab","Bb", "C", "Db", "Eb", "F", "G"],
+        "Eb-Major": ["Eb","F", "G", "Ab", "Bb", "C", "D"],
+        "Bb-Major": ["Bb","C", "D", "Eb", "F", "G", "A"],
+    ]
+    
+    var keyRelative: [String: String] = [
+        "C-Major": "A-Minor",
+        "G-Major": "E-Minor",
+        "D-Major": "B-Minor",
+        "A-Major": "F#-Minor",
+        "E-Major": "C#-Minor",
+        "B-Major": "G#-Minor",
+        "F#-Major": "D#-Minor",
+        "Db-Major": "Bb-Minor",
+        "Ab-Major": "F-Minor",
+        "Eb-Major": "C-Minor",
+        "Bb-Major": "G-Minor",
+        "F-Major": "D-Minor"
+    ]
+    
+    func getKey(notesArray: [String]) -> String {
+        
+        let notesSet = Set(notesArray)
+        
+        
+        for(key, notes) in keyList{
+            let keyListSet = Set(notes)
+            if notesSet.isSubsetOf(keyListSet)
+            {
+               return key
+            }
+        
+        }
+        
+        //if not exact match then determine by number of # or b
+        var countArray = [Int]()
+        
+        var keys = keyList.keys.sort()
+        
+        for i in 0 ..< keys.count {
+            var count = 0
+            let notes = keyList[keys[i]]
+            
+            for note in notesArray
+            {
+                if notes!.contains(note) { count += 1 }
+            }
+            countArray.append(count);
+            
+        }
+        
+        var max = countArray.maxElement()!
+        
+        var result = keys[countArray.indexOf(max)!]
+        countArray.removeAtIndex(countArray.indexOf(max)!)
+        
+        while countArray.indexOf(max) > 0 {
+            result += " or \(keys[countArray.indexOf(max)!])"
+            countArray.removeAtIndex(countArray.indexOf(max)!)
+        
+        }
+        
+        return result
+    }
+    
+    func getRelative(key: String) -> String{
+        return keyRelative[key]!
+    }
+}
--- Tuneify/ViewController.swift
+++ Tuneify/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 import Beethoven
 import Pitchy
+import AVFoundation
+
+var inputSound: AVAudioPlayer!
 
 class ViewController: UIViewController, PitchEngineDelegate {
     
@@ -16,20 +19,58 @@ class ViewController: UIViewController, PitchEngineDelegate {
         bufferSize: 4096,
         transformStrategy: .FFT,
         estimationStrategy: .MaxValue,
-        audioURL: nil))
+        audioURL: NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("Test.m4a", ofType:nil)!)))
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.pitchEngine.levelThreshold = -20.0
         self.pitchEngine.delegate = self
+        
+        let key = KeyRetriever()
+        
+        let ret = key.getKey(["F#","G#", "A", "B", "C#", "D#", "E#"])
+        print(ret)
+        
         // Do any additional setup after loading the view, typically from a nib.
+        
+       // let dirPaths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory,
+         //                                                  .UserDomainMask, true)
+        
+        //let docsDir = NSBundle.mainBundle().resourcePath!
+        //print(docsDir)
+        
+//        var bombSoundEffect: AVAudioPlayer!
+        
+//        let docsDir = dirPaths[0] as! String
+//        print(docsDir)
     }
 
     @IBAction func stop(sender: AnyObject) {
         self.pitchEngine.stop()
+        
+        if inputSound != nil {
+            inputSound.stop()
+            inputSound = nil
+        }
     }
     
     @IBAction func start(sender: AnyObject) {
+        
+        let path = NSBundle.mainBundle().pathForResource("Test.m4a", ofType:nil)!
+        let url = NSURL(fileURLWithPath: path)
+        
+        do {
+            let sound = try AVAudioPlayer(contentsOfURL: url)
+            inputSound = sound
+            //sound.play()
+        } catch {
+            // couldn't load file :(
+            print ("Error loading file")
+        }
+        
         self.pitchEngine.start()
+        
+        
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -43,6 +84,7 @@ class ViewController: UIViewController, PitchEngineDelegate {
     
     func pitchEngineDidRecieveError(pitchEngine: PitchEngine, error: ErrorType) {
         //Delegate
+        print (error)
     }
 
 
